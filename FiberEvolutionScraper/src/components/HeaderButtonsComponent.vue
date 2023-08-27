2bf0ebf434feb8a66d60df1ab3fc733c9dce331a
Inroma <romain.foray@gmail.com>
2023-08-27 15:05:33 +0200

    added tooltip

--- FiberEvolutionScraper/src/components/HeaderButtonsComponent.vue
+++ FiberEvolutionScraper/src/components/HeaderButtonsComponent.vue
@@ -2,19 +2,56 @@
     <v-container>
         <v-row dense>
             <v-col>
-                <v-btn @click="updateFibers()" color="green lighten-1" :loading="loading">Actualiser zone étendue</v-btn>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn @click="updateFibers()" color="green lighten-1" :loading="loading">
+                            Actualiser zone étendue
+                            <v-icon small color="grey lighten-2" v-on="on" right>mdi-information-outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Requête une grande zone de données sur l'API Orange <br/>
+                        pour les mettre à jour en BDD</span>
+                </v-tooltip>
             </v-col>
             <v-col>
-                <v-btn @click="getDbFibers()">Charger fibres en BDD</v-btn>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn @click="getDbFibers()">Charger fibres en BDD
+                            <v-icon small color="grey lighten-2" v-on="on" right>mdi-information-outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Affiche les raccordements fibres enregistrés en BDD</span>
+                </v-tooltip>
             </v-col>
             <v-col>
-                <v-btn @click="getFibers()" color="primary" :loading="loading">Charger zone étendue</v-btn>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn @click="getFibers()" color="primary" :loading="loading">Charger zone étendue
+                            <v-icon small color="grey lighten-2" v-on="on" right>mdi-information-outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Affiche les données de raccordement depuis <br/> l'API Orange sur une zone large</span>
+                </v-tooltip>
             </v-col>
             <v-col>
-                <v-btn @click="getCloseAreaFibers()" color="primary" :loading="loading">Charger zone proche</v-btn>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn @click="getCloseAreaFibers()" color="primary" :loading="loading">Charger zone proche
+                            <v-icon small color="grey lighten-2" v-on="on" right>mdi-information-outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Affiche les données de raccordement depuis <br/> l'API Orange sur une zone restreinte</span>
+                </v-tooltip>
             </v-col>
             <v-col>
-                <v-btn @click="getNewestFibers()" color="blue accent-4">Charger nouveaux points</v-btn>
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on }">
+                        <v-btn @click="getNewestFibers()" color="blue accent-4">Charger nouveaux points
+                            <v-icon small color="grey lighten-2" v-on="on" right>mdi-information-outline</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Affiche les points ajoutés/modifiés depuis moins de 5 jours</span>
+                </v-tooltip>
             </v-col>
         </v-row>
     </v-container>
